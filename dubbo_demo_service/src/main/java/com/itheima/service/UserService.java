d4a1d429f64a78b0c839c8c7527efad36284faed
duanduan <779368450@qq.com>
2019-05-15 17:30:46 +0800

    提交啦

---
+++ dubbo_demo_service/src/main/java/com/itheima/service/UserService.java
@@ -0,0 +1,16 @@
+package com.itheima.service;
+
+/**
+ * dubbo-生产者入门-业务逻辑接口
+ * @author Steven
+ * @version 1.0
+ * @description com.itheima.service
+ * @date 2019-4-8
+ */
+public interface UserService {
+    /**
+     * 获取用户名
+     * @return
+     */
+    public String getUserName();
+}
